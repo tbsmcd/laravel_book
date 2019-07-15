80b8b493a9a7cd798e4d34ddccc54f0e754d71c1
tbsmcd <tbsmcd@gmail.com>
2019-07-15 18:14:35 +0900

    1-3-5 1. ユーザ登録のルーティング追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,5 +4,11 @@
 </head>
 <body>
 こんにちは
+@if (Auth::check())
+    {{ \Auth::user()->name }} さん
+@else
+    ゲストさん<br />
+    <a href="/auth/register">会員登録</a>
+@endif
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/home', function() {
    return view('home');
 });
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
