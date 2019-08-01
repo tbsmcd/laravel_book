79d84c710208dcfce5f200443ce818d091ec8200
tbsmcd <tbsmcd@gmail.com>
2019-08-01 09:23:38 +0900

    5-5-2-2 リファクタリング

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\DataProvider\FavoriteRepository;
+use App\DataProvider\FavoriteRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            FavoriteRepositoryInterface::class,
+            FavoriteRepository::class
+        );
     }
 
     /**
--- app/Services/FavoriteService.php
+++ app/Services/FavoriteService.php
@@ -1,31 +1,19 @@
 <?php
 namespace App\Services;
 
-use App\DataProvider\Eloquent\Favorite;
-use Illuminate\Support\Facades\DB;
+use App\DataProvider\FavoriteRepositoryInterface;
 
 class FavoriteService
 {
+    private $favorite;
+
+    public function __construct(FavoriteRepositoryInterface $favorite)
+    {
+        $this->favorite = $favorite;
+    }
+
     public function switchFavorite(int $bookId, int $userId, string $createdAt) : int
     {
-        return DB::transaction(
-            function () use ($bookId, $userId, $createdAt) {
-                $count = Favorite::where('book_id', $bookId)
-                    ->where('user_id', $userId)
-                    ->count();
-                if ($count == 0) {
-                    Favorite::create([
-                        'book_id' => $bookId,
-                        'user_id' => $userId,
-                        'created_at' => $createdAt
-                    ]);
-                    return 1;
-                }
-                Favorite::where('book_id', $bookId)
-                    ->where('user_id', $userId)
-                    ->delete();
-                return 0;
-            }
-        );
+        return $this->favorite->switch($bookId, $userId, $createdAt);
     }
 }
