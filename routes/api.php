c6c26f0c9308f48d3708abcd2ba4545e180a98e7
tbsmcd <tbsmcd@gmail.com>
2019-08-21 22:57:39 +0900

    fix typo

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/action/favorite', 'FavoriteActionController@switchFavorite');
-Rourw::get('/payload', 'ArticlePayloadAction');
+Route::get('/payload', 'ArticlePayloadAction');
