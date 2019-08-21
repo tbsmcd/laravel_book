db761f50a6be11a869425cd236e23b25db03b20f
tbsmcd <tbsmcd@gmail.com>
2019-08-21 23:11:05 +0900

    4.4 ミドルウェア

---
+++ app/Http/Middleware/HeaderDumper.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class HeaderDumper
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $lopgger;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request instanceof Request) {
+            $this->logger->info('request', [
+               'header' => strval($request->headers)
+            ]);
+        }
+        $response = $next($request);
+        // レスポンスヘッダのログ出力
+        if ($response instanceof Response) {
+            $this->logger->info('response', [
+                'header' => strval($response->headers)
+            ]);
+        }
+        return $response;
+    }
+}
