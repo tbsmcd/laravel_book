6b28c67faee6ef9ec86e6577f7d604c7d421de14
tbsmcd <tbsmcd@gmail.com>
2019-08-21 22:19:36 +0900

    4.3 ArticlePayloadAction

---
+++ app/Http/Controllers/ArticlePayloadAction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ArticleResource;
+use Illuminate\Http\Request;
+
+final class ArticlePayloadAction extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $resource = new ArticleResource([
+            'id' => 1,
+            'title' => 'Laravel rest api',
+            'comments' => [
+                [
+                    'id' => 2134,
+                    'body' => 'test',
+                    'user_id' => 10000,
+                    'user_name' => 'tbsmcd',
+                ]
+            ],
+            'user_id' => 2000,
+            'user_name' => 'user1'
+        ]);
+        return $resource->response($request)->header('content-type', 'application-json');
+    }
+}
