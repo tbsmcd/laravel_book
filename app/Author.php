c3adc8ad5127199b252afc74953e58854a92a013
tbsmcd <tbsmcd@gmail.com>
2019-11-07 00:12:54 +0900

    ひとまずここまで

--- app/Author.php
+++ app/Author.php
@@ -5,6 +5,33 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Author
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $kana
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Book[] $books
+ * @property-read int|null $books_count
+ * @method static bool|null forceDelete()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author newQuery()
+ * @method static \Illuminate\Database\Query\Builder|\App\Author onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author query()
+ * @method static bool|null restore()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereKana($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Author whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Author withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\App\Author withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Author extends Model
 {
     use SoftDeletes;
--- app/DataProvider/Eloquent/Favorite.php
+++ app/DataProvider/Eloquent/Favorite.php
@@ -4,6 +4,22 @@ namespace App\DataProvider\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\DataProvider\Eloquent\Favorite
+ *
+ * @property int $book_id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\DataProvider\Eloquent\Favorite whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Favorite extends Model
 {
     protected $fillable = [
