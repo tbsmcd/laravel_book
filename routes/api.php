05c8e37dbdb46d439a715a92a3e37490e801965b
tbsmcd <tbsmcd@gmail.com>
2019-11-06 23:19:14 +0900

    other tests

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,19 @@ use Illuminate\Http\Request;
 Route::get('customers', function() {});
 Route::post('customers', function() {});
 
+Route::get('customers/{customer_id}', function() {});
+Route::post('customers/{customer_id}', function() {});
+Route::put('customers/{customer_id}', function() {});
+Route::delete('customers/{customer_id}', function() {});
+
+Route::get('reports', function() {});
+Route::post('reports', function() {});
+
+Route::get('reports/{report_id}', function() {});
+Route::post('reports/{report_id}', function() {});
+Route::put('reports/{report_id}', function() {});
+Route::delete('reports/{report_id}', function() {});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -25,4 +25,95 @@ class ReportTest extends TestCase
         $response = $this->post('api/customers');
         $response->assertStatus(200);
     }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_id_get_access_able()
+    {
+        $response = $this->get('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_id_post_access_able()
+    {
+        $response = $this->post('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_id_put_access_able()
+    {
+        $response = $this->put('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_id_delete_access_able()
+    {
+        $response = $this->delete('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_get_access_able()
+    {
+        $response = $this->get('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_post_access_able()
+    {
+        $response = $this->post('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_id_get_access_able()
+    {
+        $response = $this->get('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_id_post_access_able()
+    {
+        $response = $this->post('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_id_put_access_able()
+    {
+        $response = $this->put('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_id_delete_access_able()
+    {
+        $response = $this->delete('api/reports/1');
+        $response->assertStatus(200);
+    }
+
 }
