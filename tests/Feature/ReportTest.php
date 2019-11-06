f344f07e1b2eac7a7b46093f2f06e14fe6432a8a
tbsmcd <tbsmcd@gmail.com>
2019-11-06 23:04:16 +0900

    first test

---
+++ tests/Feature/ReportTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReportTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function api_test_access_able()
+    {
+
+        $response->assertStatus(200);
+    }
+}
