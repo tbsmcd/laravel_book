690185627e6342f6ee03c80925c5b0a903ede8dd
tbsmcd <tbsmcd@gmail.com>
2019-11-06 23:08:52 +0900

    first success

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
+// 11章
+Route::get('customers', function() {});
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
